5eca0d5730352d9068c2ed2ec317e9fd938b3944
AlexisTrinh <phuc.trinh91@gmail.com>
2022-02-07 11:51:32 -0700

    Update launch.bat

--- launch.bat
+++ launch.bat
@@ -1,1 +1,3 @@
-InsurgencyServer.exe Oilfield?Scenario=Scenario_Refinery_Checkpoint_Security?MaxPlayers=28 -Port=27102 -QueryPort=27131 -log -hostname="Server" -Mods -Rcon -RconPassword=password -RconListenPort=27015 -EnableCheats -ModDownloadTravelTo=Oilfield?Scenario=Scenario_Refinery_Checkpoint_Security
+@echo off
+cd /d "%~dp0"
+start /b InsurgencyServer.exe Oilfield?Scenario=Scenario_Refinery_Checkpoint_Security?MaxPlayers=28 -Port=27102 -QueryPort=27131 -log -hostname="Server" -Mods -Rcon -RconPassword=password -RconListenPort=27015 -EnableCheats -ModDownloadTravelTo=Oilfield?Scenario=Scenario_Refinery_Checkpoint_Security
